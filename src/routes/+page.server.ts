e7d51b25ad68dea10ceae2398069289cbb30bc59
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-11-16 21:17:00 +0800

    Delete src directory

--- src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { superValidate } from 'sveltekit-superforms/client';
-import { schemaCreate, schemaEdit } from './schema';
-
-export async function load() {
-	const formCreate = await superValidate(schemaCreate, { id: 'create' });
-	const formEdit = await superValidate(schemaEdit, { id: 'edit' });
-	return { formCreate, formEdit };
-}
